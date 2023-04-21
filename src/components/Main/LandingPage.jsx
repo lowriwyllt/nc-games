17e9f8e0d6d4b8d7f220758c13130476c970be84
lowriwyllt <lowriwyllt@live.co.uk>
2023-04-21 09:51:29 +0100

    makes clicking suggested '...yes' or '...no' to not cause an error

--- src/components/Main/LandingPage.jsx
+++ src/components/Main/LandingPage.jsx
@@ -11,18 +11,23 @@ const LandingPage = () => {
   //handles when someone types in "y" or "n"
   let navigate = useNavigate();
   const handleOnChange = (event) => {
-    if (event.nativeEvent.data.toLowerCase() === "y") {
-      setContinueValue("... yes");
-      setTimeout(() => {
-        navigate("/reviews");
-      }, 750);
-      setActiveNavbar(false);
-    } else if (event.nativeEvent.data.toLowerCase() === "n") {
-      setContinueValue("... no");
-      setTimeout(() => {
-        navigate("/noEntry");
-      }, 750);
-      setActiveNavbar(false);
+    console.dir(event);
+    if (event.nativeEvent.data) {
+      if (event.nativeEvent.data.toLowerCase() === "y") {
+        console.log("Im a 'y'");
+        setContinueValue("... yes");
+        setTimeout(() => {
+          navigate("/reviews");
+        }, 750);
+        setActiveNavbar(false);
+      } else if (event.nativeEvent.data.toLowerCase() === "n") {
+        console.log("Im a 'n'");
+        setContinueValue("... no");
+        setTimeout(() => {
+          navigate("/noEntry");
+        }, 750);
+        setActiveNavbar(false);
+      }
     }
   };
 
